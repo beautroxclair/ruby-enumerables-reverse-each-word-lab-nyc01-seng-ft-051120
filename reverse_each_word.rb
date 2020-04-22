7adf71b45ba73ab1fc8c8855a8ce1b355c50b449
Beau Troxclair <beau.troxclair@gmail.com>
2020-04-22 04:30:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(str)
   arr = str.split(' ')
-  arr_new = arr.each do |el|
-    el.reverse
+  arr_new = []
+  arr.each do |el|
+    arr_new.push(el.reverse)
   end
   arr_new.join(' ')
 end
